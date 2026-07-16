5af00398f4475ccc88ec7c80842604fb44d1deaf
Antonio Delvair Zaneti <antonio.zaneti@gmail.com>
2026-07-16 12:11:03 -0300

    Refactor EstudosJava to Main with new operations
    Updated the main class and added operations for addition and subtraction.

--- EstudosJava/src/estudosjava/EstudosJava.java
+++ EstudosJava/src/estudosjava/EstudosJava.java
@@ -1,31 +1,38 @@
-package estudosjava;
+package org.example;
 import java.util.Scanner;
-/**
- *
- * @author Agência Zaneti
- */
-public class EstudosJava {
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+
+public class Main {
+    public static void main(String[]     args){
         // TODO code application logic here
         Scanner reader =new Scanner(System.in);
-        
-        System.out.println("What is The Number?");
-        String num= reader.nextLine();
-        int a=Integer.parseInt(num);
-        switch (a)
-        {
-            case 1:
-                System.out.println("Correct!"+a);
+
+        System.out.println("Digite o numero da operacao a ser realizada: \n 1- Adicao \n2-Subtracao \n3-Divisao \n4-Multiplicacao");
+        String op= reader.nextLine();
+        switch(op){
+            case "1":{
+
+                System.out.println("Digite o primeiro numero");
+
+                int num1 = reader.nextInt();
+
+                System.out.println("Digite o segundo numero");
+                int num2 = reader.nextInt();
+                int soma = num1+num2;
+                System.out.printf ("A somatoria de %d e %d é %d %n", num1, num2, soma);
                 break;
-            case 2:
-                System.out.println("Erro"+a);
+            }
+            case "2": {
+                System.out.println("Digite o número inicial");
+                int num1 = reader.nextInt();
+                System.out.println("Digite o numero a ser subtraido");
+                int num2 = reader.nextInt();
+                int subtracao = num1 - num2;
+                System.out.printf("A subtracao de %d - %d é %d %n", num1, num2, subtracao);
                 break;
+            }
             default:
-                    System.out.println("Incorrect!"+a);
-                    break;
+                System.out.println("Opcao Invalida!");
         }
+        reader.close();
     }
 }
